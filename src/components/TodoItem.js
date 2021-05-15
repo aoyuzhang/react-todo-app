3f1549a5fcb81d1bcd7ba3115df726251b092e4a
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-14 18:09:51 -0700

    rearranged codes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,12 @@ import React from "react"
 function TodoItem(props) {
   return (
     <li>
-      <input type="checkbox" checked={props.todo.completed}/>{props.todo.title}
+      <input
+        type="checkbox"
+        checked={props.todo.completed}
+        onChange={() => this.props.handleChangeProps()}
+      />
+        {props.todo.title}
     </li>
   )
 }
