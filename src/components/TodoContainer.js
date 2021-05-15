f89a0efcf78d067c5a450960234b79ad3494f412
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-14 20:09:35 -0700

    update

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from "react"
 import TodosList from "./TodosList";
 import Header from "./Header"
+import InputTodo from "./InputTodo"
 
 
 class TodoContainer extends React.Component {
@@ -41,13 +42,20 @@ class TodoContainer extends React.Component {
   }
 
   delTodo = id => {
-    console.log("deleted", id);
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    })
   }
 
   render() {
     return (
       <div>
         <Header/>
+        <InputTodo />
         <TodosList
           todos={this.state.todos}
           handleChangeProps = {this.handleChange}
