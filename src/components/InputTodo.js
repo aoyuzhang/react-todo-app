98f83e4804c6c18fc840c9c236147d1ec0d971e5
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-14 20:33:53 -0700

    update

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -13,8 +13,11 @@ class InputTodo extends Component{
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state.title);
-  }
+    this.props.addTodoProps(this.state.title);
+    this.setState({
+      title: ""
+    });
+  };
 
   render(){
     return(
